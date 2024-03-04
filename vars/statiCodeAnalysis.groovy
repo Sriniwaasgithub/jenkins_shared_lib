39973a3a552c6ac8a6a9febca81c557c0d4224a2
Sriniwaasgithub <114711537+Sriniwaasgithub@users.noreply.github.com>
2024-03-04 11:06:10 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,8 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-        sh sonar.sources=src/main/java
-        sh sonar.java.binaries=target/classes
         sh 'mvn clean package sonar:sonar'
     }
 }
